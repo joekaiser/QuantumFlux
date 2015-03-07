dfa9fe2fce1387163194d24ad8d3b9c4d92d52fa
Joe Kaiser <joe.kaiser@slalom.com>
2015-03-07 09:05:14 -0800

    setting version to 1.0

--- src/main/java/jotato/quantumflux/Reference.java
+++ src/main/java/jotato/quantumflux/Reference.java
@@ -3,5 +3,5 @@ package jotato.quantumflux;
 public class Reference {
 	public static final String MODNAME = "QuantumFlux";
     public static final String MODID = "quantumflux";
-    public static final String VERSION = "0.1-a";
+    public static final String VERSION = "1.0.0";
 }
