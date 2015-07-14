846b0be858e30340ab5fe957447b299799ffa91a
Joe Kaiser <jotato@zoho.com>
2015-07-14 10:15:07 -0700

    version 1.3.0-A2

--- src/main/java/jotato/quantumflux/Reference.java
+++ src/main/java/jotato/quantumflux/Reference.java
@@ -3,5 +3,5 @@ package jotato.quantumflux;
 public class Reference {
 	public static final String MODNAME = "QuantumFlux";
     public static final String MODID = "quantumflux";
-    public static final String VERSION = "1.7.10-1.3.0-A1";
+    public static final String VERSION = "1.7.10-1.3.0-A2";
 }
