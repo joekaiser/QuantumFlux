c9fea6b4c42bc8b5d69d4f5801cebf65a729131f
Joe Kaiser <jotato@zoho.com>
2016-02-12 13:53:11 -0800

    version bump

--- src/main/java/jotato/quantumflux/QuantumFluxMod.java
+++ src/main/java/jotato/quantumflux/QuantumFluxMod.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class QuantumFluxMod {
 	public static final String MODID = "quantumflux";
 	public static final String MODNAME = "QuantumFlux";
-	public static final String VERSION = "2.0.0-b2";
+	public static final String VERSION = "2.0.0-b3";
 	public static final String TEXTURE_BASE = MODID + ":";
 
 
